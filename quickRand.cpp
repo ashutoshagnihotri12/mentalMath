2f2af8a16cbcd43e38938232a894515e67528a1b
Ashutosh Agnihotri <agnihotr@cwi.nl>
2017-09-12 17:50:22 +0200

    Experimentation with random Numbers

--- quickRand.cpp
+++ quickRand.cpp
@@ -4,7 +4,14 @@
 int main(){
 	std::srand(std::time(0)); // use current time as seed for random generator
 	int randVar = std::rand();
+	int randVar2 = std::rand();
+	double u = 0.0;
 	std::cout << "Random value on [0 " << RAND_MAX << "]: " << randVar <<'\n';
 	//std::cout << "Random value on [0 " << RAND_MAX << "]" <<'\n';
-	//std::cout << randVar <<'\n';
+	for (int i=0; i<10; ++i){
+	randVar = std::rand();
+	randVar2 = std::rand();
+	u = (double)std::rand()/(double)RAND_MAX;
+	std::cout << u <<" and "<< randVar2 <<'\n';
+	}
 }
